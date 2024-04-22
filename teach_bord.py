8b30ef587e7ee10e3b8ad088afe90c3812d8dc29
abdou-ilmi <ilmiamirabdoulkarim@gmail.com>
2024-04-22 23:34:36 +0300

    modification4

--- teach_bord.py
+++ teach_bord.py
@@ -123,16 +123,16 @@ def main():
     # Utiliser les colonnes pour afficher les images côte à côte
     #col1, col2 = st.columns(2)
     
-    # Afficher l'image 1 dans la première colonne
-    with col1:
-        st.image("/home/ilham/Documents/tab_de_bord_teach/image_ece.PNG", width=100, caption='', use_column_width=False)
-      # Afficher un espace vide entre les colonnes
-    col2.empty()   
+    # # Afficher l'image 1 dans la première colonne
+    # with col1:
+    #     st.image("/home/ilham/Documents/tab_de_bord_teach/image_ece.PNG", width=100, caption='', use_column_width=False)
+    #   # Afficher un espace vide entre les colonnes
+    # col2.empty()   
          
-    # Afficher l'image 2 dans la deuxième colonne
-    with col3:
-        st.image("/home/ilham/Documents/tab_de_bord_teach/image_edu.png", width=100, caption='', use_column_width=False,
-                  output_format='auto')
+    # # Afficher l'image 2 dans la deuxième colonne
+    # with col3:
+    #     st.image("/home/ilham/Documents/tab_de_bord_teach/image_edu.png", width=100, caption='', use_column_width=False,
+    #               output_format='auto')
 #################################
     # col1, _, col2, _, col3 = st.columns([1, 1.5, 1, 1.5, 1])
     #  # Utiliser les colonnes pour afficher les images côte à côte
